218756ad7e30ef53bebce52d36cbf7969785ba51
Raydel Conesa Perez <rat.esp.kone@gmail.com>
2024-07-03 19:18:27 +0200

    addind sierra nevada product

--- src/hooks/SliderReact.jsx
+++ src/hooks/SliderReact.jsx
@@ -4,22 +4,27 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 
 // import required modules
-import { Pagination } from 'swiper/modules';
+import { Pagination, Navigation } from 'swiper/modules';
 
 export default function App({IMG}) {
  
    return (
     <>
       <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        loop={true}
         pagination={{
-          dynamicBullets: true,
+          clickable: true,
         }}
-        key="2"
-        modules={[Pagination]}
-        className="mySwiper rounded-3xl mx-8 " 
-      >  
+        navigation={true}
+        modules={[Pagination, Navigation]}
+        className="mySwiper rounded-3xl mx-8 "
+        
+      >
        {IMG.map(function(i, index) {
           return <SwiperSlide key={index}><img className="m-auto h-[250px] md:h-[550px]  object-cover w-full" src={i.src}/></SwiperSlide>;
         })}      
